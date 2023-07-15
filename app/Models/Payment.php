90fe4160e9b9a5bc57d0d2003f59a5ec127fe9ac
Sourav <souravsikun26@gmail.com>
2023-07-15 20:49:46 +0530

    finaal commit

---
+++ app/Models/Payment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'rental_id',
+        'amount',
+        'payment_amount',
+        'payment_method',
+    ];
+}
---
+++ database/migrations/2023_05_26_122910_create_rentals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rentals', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('gadget_id');
+            $table->foreign('gadget_id')->references('id')->on('gadgets');
+            $table->date('rental_date');
+            $table->date('return_date');
+            $table->float('rental_price');
+            $table->bigInteger('payment_id');
+            $table->foreign('payment_id')->references('id')->on('payments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rentals');
+    }
+};
